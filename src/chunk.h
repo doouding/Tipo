842f1d1a668f6fcc76c5e0c36c068f20f4cddc9b
codert <codert.sn@gmail.com>
2021-09-20 16:26:59 +0800

    perf: add basic initial code

--- src/chunk.h
+++ src/chunk.h
@@ -1,10 +1,19 @@
-#ifdef tipo_chunk_h
+#ifndef tipo_chunk_h
 #define tipo_chunk_h
 
 #include "common.h"
-
 typedef enum {
   OP_RETURN,
 } OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
 
 #endif
